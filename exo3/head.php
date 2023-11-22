1c0fa33cdf990d9e96c3f96eb628b2fe163de9d2
tim <timdesmet00@gmail.com>
2023-11-22 11:58:11 +0100

    amélioration de l'exo3

--- exo3/head.php
+++ exo3/head.php
@@ -2,11 +2,11 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="fr"> <!--lang= langue utilisée pour la page-->
+<html lang="en"> <!--lang= langue utilisée pour la page-->
     <head>
-        <title></title>  <!--titre de la page affiché dans l'onglet navigateur-->
+        <title>useCase#3</title>  <!--titre de la page affiché dans l'onglet navigateur-->
         <meta charset="utf-8"> <!--cryptage de la page pour facilité la lecture par le moteur de recherche-->
-        <meta name="description" content="">    <!--courte description du contenu de la page-->
+        <meta name="description" content="exercice 3 du bootcamps orienté object chez BeCode">    <!--courte description du contenu de la page-->
         <meta name="keywords" content="">  <!--mot cléf pour le moteur de recherche: keyword1, keyword2, ...-->
         <meta name="author" content="Desmet Tim">   <!--auteur de la page: auteur1, auteur2, ...-->
         <meta name="viewport" content="width=device-width, initial-scale=1">  <!--pour garder les proportions sur différent affichage-->
